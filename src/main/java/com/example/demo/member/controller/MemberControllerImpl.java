78f0ce4f2fbe977d77472168ff4a25837d163b1a
ypk0680 <ypk0680@naver.com>
2025-05-08 15:31:19 +0900

    Merge remote-tracking branch 'final/main'

--- src/main/java/com/example/demo/member/controller/MemberControllerImpl.java
+++ src/main/java/com/example/demo/member/controller/MemberControllerImpl.java
@@ -52,7 +52,7 @@ public class MemberControllerImpl implements MemberController{
 		// TODO Auto-generated method stub
 		MemberDTO dto = service.findById(id);
 		model.addAttribute("member", dto);
-		return "/member/detailMember";
+		return "member/detailMember";
 	}
 
 	@Override
@@ -100,7 +100,7 @@ public class MemberControllerImpl implements MemberController{
 			model.addAttribute("message", "아이디나 암호가 잘못 되었습니다. 다시 로그인하세요.");
 			model.addAttribute("redirectUrl", "/member/loginForm");
 		}
-		return "/common/alert";
+		return "common/alert";
 	}
 
 	@Override
@@ -111,7 +111,7 @@ public class MemberControllerImpl implements MemberController{
 		session.invalidate();
 		model.addAttribute("message", loginId + "님이 로그아웃 하셨습니다.");
 		model.addAttribute("redirectUrl", "/member/loginForm");
-		return "/common/alert";
+		return "common/alert";
 	}
 	
 	
